05d5c3654f974324741144599986e505f80a2181
Stephen Yang <kwyang@gmail.com>
2021-03-29 14:24:35 +0800

    int solution(vector<int> &A) {

---
+++ 03_TimeComplexity/PermMissingElem/solution.cpp
@@ -0,0 +1,18 @@
+// https://app.codility.com/demo/results/training7XQSZ2-7DD/
+
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+
+    uint64_t n = A.size();
+    uint64_t calculated_sum = (1+n+1)*(n+1)/2;
+    uint64_t curr_sum = 0;
+
+    for (int i:A)
+        curr_sum += i;
+
+    return calculated_sum-curr_sum;
+}
